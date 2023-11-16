e45894dba7e3d4b1b464de5c8495590439888552
ksb <qkfka9045@gmail.com>
2023-11-16 10:57:35 +0900

    feat: 마커 최소 개수 제한

--- src/main/java/com/ssafy/realty/realty/adapter/in/web/payload/wrap/MarkerPayloads.java
+++ src/main/java/com/ssafy/realty/realty/adapter/in/web/payload/wrap/MarkerPayloads.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MarkerPayloads {
+    @Size(min = 1)
     List<MarkerPayload> markers;
 }
